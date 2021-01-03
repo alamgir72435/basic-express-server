80b322e17030d09af8ceaa53ddcde65f94532d7d
agradut <agradut72435@gmail.com>
2021-01-03 10:01:11 +0600

    implement express-handlebars

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const exphbs = require("express-handlebars")
+
+
 const app = express()
+
+// Middleware
+app.engine("handlebars", exphbs())
+app.set("view engine", "handlebars")
+
 const port= 5000;
 
 
 app.get('/', (req,res)=> {
-	res.send('you are in our express server')
+	res.render("home")
 })
 
+app.get('/about', (req, res) => {
+	res.render("about")
+})
 
-app.listen(port,console.log('server running'))
+	
+app.listen(port,console.log('server running '+port))
