bb676d87ac8c61595ba7ffdff348925404ebea95
agradut <agradut72435@gmail.com>
2021-01-05 10:10:38 +0600

    post request handle user from create and view user list

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require('express')
 const exphbs = require("express-handlebars")
-
-
+const bodyParser = require("body-parser")
+const port= 5000;
 const app = express()
+const users = []
 
 // Middleware
 app.engine("handlebars", exphbs())
 app.set("view engine", "handlebars")
 
-const port= 5000;
+// Body parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 
+
+// Get Request Handle
 app.get('/', (req,res)=> {
 	res.render("home")
 })
 
-app.get('/about', (req, res) => {
+app.get('/allusers',(req,res) => {
+	res.render('users', {
+		users
+	})
+})
+
+app.get('/about',  (req, res) => {
 	res.render("about")
 })
 
+// Post Request Handle
+app.post('/user',(req, res) => {
+	const { name, phone } = req.body
+	let user = { name:name, phone:phone }
+	users.push(user)
+	res.redirect('/allusers')
+})
 	
 app.listen(port,console.log('server running '+port))
