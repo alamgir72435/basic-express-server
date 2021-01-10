1097cca455e9e6766d9b1e7761597fa8c9c5a414
agradut <agradut72435@gmail.com>
2021-01-10 10:22:07 +0600

    mongodb connection

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const port= 5000;
 const app = express()
 const users = []
-
+const uri = 'mongodb+srv://agradut:agradut@cluster0.dziq6.mongodb.net/mysite?retryWrites=true&w=majority'
 // Middleware
 app.engine("handlebars", exphbs())
 app.set("view engine", "handlebars")
+
+// Database Connection  (MongoDB)
+mongoose.connect(uri, {
+	useNewUrlParser:true,
+	useUnifiedTopology:true
+}).then(() => {
+	console.log(`MongoDB Connected`)
+}).catch((error) => {
+	console.log(error)
+})
+
+
+// Data Format / Schema 
+// Schema 
+const messageSchema = new mongoose.Schema({
+	name:{type:String},
+	email:{type:String},
+	subject:{type:String},
+	message:{type:String}
+})
+
+// Model 
+const Message = mongoose.model('messages',messageSchema)
+
+
 
 // Body parser
 app.use(bodyParser.urlencoded({extended:false}))
@@ -32,10 +58,26 @@ app.get('/about',  (req, res) => {
 
 // Post Request Handle
 app.post('/user',(req, res) => {
-	const { name, phone } = req.body
-	let user = { name:name, phone:phone }
-	users.push(user)
-	res.redirect('/allusers')
+	const { name, email, subject, message } = req.body
+
+	const user = new Message({
+		name,
+		email,
+		subject,
+		message
+	})
+
+	user.save().then(newUser => {
+		if(newUser){
+			res.redirect('/allusers')
+		}
+	}).catch(err => {
+		console.log('Something went Wrong !')
+	})
+
+
+	
+	
 })
 	
 app.listen(port,console.log('server running '+port))
